cf4ff2eb08f95cdff4ce5223dd8a5df22b578e8b
artemgon <artemhoncharuk123@gmail.com>
2025-01-03 20:04:59 +0200

    Task 3 is done.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 
-bool perfSquare(int n)
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Choose the way you want to sort the array:");
+Console.WriteLine("1. Ascending");
+Console.WriteLine("2. Descending");
+Console.Write("Enter your choice: ");
+Console.WriteLine("True for Ascending, False for Descending");
+bool choice = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine();
+
+int[] arr = { 5, 2, 8, 6, 1, 9, 3, 7, 4 };
+
+int[] ArraySort(int[] arr)
 {
-    int s = (int)Math.Sqrt(n);
-    return (s * s == n);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (choice)
+            {
+                if (arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            else
+            {
+                if (arr[i] < arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    return arr;
 }
 
-bool fib(int n)
+int[] sortedArray = ArraySort(arr);
+for (int i = 0; i < sortedArray.Length; i++)
 {
-    return perfSquare(5 * n * n + 4) || perfSquare(5 * n * n - 4);
-}
-
-int myNumber = 55;
-if (fib(myNumber))
-{
-    Console.WriteLine($"{myNumber} is a Fibonacci number");
-}
-else
-{
-    Console.WriteLine($"{myNumber} is not a Fibonacci number");
+    Console.Write(sortedArray[i] + " ");
 }
