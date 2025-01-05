310a5388a4ae0689a8c64bfbbd944bbdcd7439c8
artemgon <artemhoncharuk123@gmail.com>
2025-01-05 19:04:48 +0200

    Task 2 is done.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿namespace project_19_11_24
+﻿using project_19_11_24.Classes;
+
+int number = int.Parse(Console.ReadLine() ?? "");
+
+bool palindrome(int number)
 {
-    public class Program
+    int temp = number;
+    int reverse = 0;
+    while (temp != 0)
     {
-        static void Main()
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            try
-            {
-            }
-            catch (Exception e)
-            {
-            }
-        }
+        int remainder = temp % 10;
+        reverse = reverse * 10 + remainder;
+        temp /= 10;
     }
+    return reverse == number;
+}
+
+try
+{ 
+    if (palindrome(number))
+    {
+        Console.WriteLine("Number is a palindrome.");
+    }
+    else
+    {
+        throw new Exception();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error... Something went wrong.");  
 }
