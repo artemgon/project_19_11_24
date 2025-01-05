f68fd1fae6bc8dcbe72d9a9f41e6ee4dd7ac4bce
artemgon <artemhoncharuk123@gmail.com>
2025-01-05 18:37:09 +0200

    Task 6 is done.

--- Classes/classes.cs
+++ Classes/classes.cs
@@ -1,7 +1,119 @@
 ﻿
 namespace project_19_11_24.Classes
 {
-    internal class Classes
+    public class Plane
     {
+        public string Name;
+        public string Manufacturer;
+        public int Year;
+        public string Type;
+        public Plane()
+        {
+            Name = "empty";
+            Manufacturer = "empty";
+            Year = 0;
+            Type = "empty";
+        }
+        public Plane(string name) : this()
+        {
+            Name = name;
+        }
+        public Plane(string name, string manufacturer) : this(name)
+        {
+            Manufacturer = manufacturer;
+        }
+        public Plane(string name, string manufacturer, int year) : this(name, manufacturer)
+        {
+            Year = year;
+        }
+        public Plane(string name, string manufacturer, int year, string type) : this(name, manufacturer, year)
+        {
+            Type = type;
+        }
+        public Plane(char[] name) : this()
+        {
+            Name = new string(name);
+        }
+        public Plane(char[] name, char[] manufacturer) : this(name)
+        {
+            Manufacturer = new string(manufacturer);
+        }
+        public Plane(char[] name, char[] manufacturer, double year) : this(name, manufacturer)
+        {
+            Year = (int)year;
+        }
+        public Plane(char[] name, char[] manufacturer, double year, char[] type) : this(name, manufacturer, year)
+        {
+            Type = new string(type);
+        }
+        // overloaded setters and getters
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public void SetName(char[] name)
+        {
+            Name = new string(name);
+        }
+        public void SetManufacturer(string manufacturer)
+        {
+            Manufacturer = manufacturer;
+        }
+        public void SetManufacturer(char[] manufacturer)
+        {
+            Manufacturer = new string(manufacturer);
+        }
+        public void SetYear(int year)
+        {
+            Year = year;
+        }
+        public void SetYear(double year)
+        {
+            Year = (int)year;
+        }
+        public void SetType(string type)
+        {
+            Type = type;
+        }
+        public void SetType(char[] type)
+        {
+            Type = new string(type);
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public char[] GetNameCharArray()
+        {
+            return Name.ToCharArray();
+        }
+        public string GetManufacturer()
+        {
+            return Manufacturer;
+        }
+        public char[] GetManufacturerCharArray()
+        {
+            return Manufacturer.ToCharArray();
+        }
+        public int GetYear()
+        {
+            return Year;
+        }
+        public string GetType()
+        {
+            return Type;
+        }
+        public char[] GetTypeCharArray()
+        {
+            return Type.ToCharArray();
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Manufacturer: {Manufacturer}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Type: {Type}");
+            Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace project_19_11_24
+﻿using project_19_11_24.Classes;
+
+namespace project_19_11_24
 {
     public class Program
     {
@@ -7,6 +9,15 @@
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             try
             {
+                Plane plane = new Plane("Boeing 747", "Boeing", 1969, "Passenger");
+                Console.WriteLine($"Name: {plane.Name}");
+                Console.WriteLine($"Manufacturer: {plane.Manufacturer}");
+                Console.WriteLine($"Year: {plane.Year}");
+                Console.WriteLine($"Type: {plane.Type}");
+                Console.WriteLine();
+                Plane plane1 = new Plane("F-22 Raptor", "Lockheed Martin", 1997, "Military jet");
+                plane1.Print();
+                Console.WriteLine();
             }
             catch (Exception e)
             {
