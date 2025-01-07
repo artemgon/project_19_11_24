f8d9c49d416d68a901628b930a7244a47f4138a9
artemgon <artemhoncharuk123@gmail.com>
2025-01-07 16:58:04 +0200

    Task 4 is done.

--- Classes/classes.cs
+++ Classes/classes.cs
@@ -1,7 +1,18 @@
 ﻿
 namespace project_19_11_24.Classes
 {
-    internal class Classes
+    public class Website
     {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string IP_Address { get; set; }
+        public void printInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Url: " + Url);
+            Console.WriteLine("Description: " + Description);
+            Console.WriteLine("IP Address: " + IP_Address);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
-﻿namespace project_19_11_24
+﻿using project_19_11_24.Classes;
+
+namespace project_19_11_24
 {
     public class Program
     {
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Website Information:");
+            Console.WriteLine();
             try
             {
+                Website website = new Website();
+                website.Name = "Google";
+                website.Url = "https://www.google.com";
+                website.Description = "Search Engine";
+                website.IP_Address = "123.456.789.0";
+                website.printInfo();
             }
             catch (Exception e)
             {
+                Console.WriteLine(e.Message);
             }
         }
     }
