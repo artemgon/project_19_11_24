fdd4571fbf9bee8d9d8de61de77602dbe4d94d99
artemgon <artemhoncharuk123@gmail.com>
2025-01-07 17:04:03 +0200

    Task 5 is created.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace project_19_11_24
+﻿using project_19_11_24.Classes;
+
+namespace project_19_11_24
 {
     public class Program
     {
@@ -7,9 +9,17 @@
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             try
             {
+                Magazine magazine = new Magazine();
+                magazine.Title = "Magazine Title";
+                magazine.Description = "Magazine Description";
+                magazine.ContactPhone = "123456789";
+                magazine.Email = "magazineemail.com";
+                magazine.CreationYear = 2020;
+                magazine.printInfo();
             }
             catch (Exception e)
             {
+                Console.WriteLine(e.Message);
             }
         }
     }
